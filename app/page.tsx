7ce01856afa21e92a609b66c778c4c54e20a7acf
Raine Soriano <rainearcher@g.ucla.edu>
2023-12-18 23:13:59 -0800

    create grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
-import Image from 'next/image'
-import Link from 'next/link'
+
+
+function LetterSquare({letter}: {letter?: string}) {
+  return <div className="square">{letter}</div>
+}
+
+function Row() {
+  return <div>
+    <LetterSquare/>
+    <LetterSquare/>
+    <LetterSquare/>
+    <LetterSquare/>
+    <LetterSquare/>
+  </div>
+}
+
+function Grid() {
+  return <div style={{display: "block"}}>
+    <Row/>
+    <Row/>
+    <Row/>
+    <Row/>
+    <Row/>
+    <Row/>
+  </div>
+}
 
 export default function Home() {
   return (
     <main>
-
+      <Grid/>
     </main>
     
   )
