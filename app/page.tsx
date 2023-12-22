f798db9c55108794502d45e2be322bfed221ee3f
Raine Soriano <rainearcher@g.ucla.edu>
2023-12-21 23:02:31 -0800

    add keyboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-
+'use client'
+import Keyboard from 'react-simple-keyboard';
+import 'react-simple-keyboard/build/css/index.css';
 
 function LetterSquare({letter}: {letter?: string}) {
   return <div className="flex-grow bg-transparent border-white border-solid border-2 text-black inline-block m-1 aspect-square rounded-md">
@@ -17,7 +19,7 @@ function Row() {
 }
 
 function Grid() {
-  return <div className="flex-col relative w-screen text-center max-w-screen-sm">
+  return <div className="flex-col relative text-center w-96">
     <Row/>
     <Row/>
     <Row/>
@@ -29,9 +31,34 @@ function Grid() {
 
 export default function Home() {
   return (
-    <div className="home">
-      <Grid/>
-    </div>
+  <div className="flex flex-wrap items-center justify-center h-screen">
+    <Grid/>
+    <Keyboard
+      theme="hg-theme-default keyboard"
+      layout={{
+        default: [
+          "Q W E R T Y U I O P",
+          "A S D F G H J K L",
+          "{enter} Z X C V B N M {bksp}"
+        ],
+        
+      }}
+      buttonTheme={[
+        {
+          class: "text-black",
+          buttons: "Q W E R T Y U I O P A S D F G H J K L Z X C V B N M {bksp} {enter}"
+        }
+      ]} 
+      display={{
+        '{bksp}': 'back',
+        '{enter}': 'enter'
+      }}
+      
+    />
+
+  </div>
+    
+    
     
   )
 }
