4e238d83f2e04eba64881bb0962017a7f5f54a93
Raine Soriano <68267367+rainearcher@users.noreply.github.com>
2024-01-10 17:15:11 -0800

    Replace Supabase with java backend (#2)
    * Move frontend code to frontend folder
    
    * Add backend code
    
    * Add randomanswer endpoint
    
    * Initialize answers list on construction
    
    * Add valid word check endpoint
    
    * Add answers.csv and words.csv
    
    * add gitignore to root dir
    
    * Modify frontend to query backend instead of supabase

---
+++ frontend/app/wordish/supabase.tsx
@@ -0,0 +1,26 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+
+export async function getRandomWord(): Promise<string> {
+    let { data: random_words, error } = await supabase
+    .from('random_answers')
+    .select('word')
+    .limit(1)
+    .single()
+
+    if (random_words != null)
+        return random_words.word;
+    else
+        return "fail";
+
+}
+
+export async function isValidWord(word: string): Promise<boolean> {
+    let { data: random_words, error } = await supabase
+    .from('random_words')
+    .select('word')
+    .eq('word', word.toLowerCase());
+    
+    return random_words != null && random_words.length != 0;
+}
