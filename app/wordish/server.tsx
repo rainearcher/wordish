b092675258ab98fb80d14b5caa38cec8aefbb549
Raine Soriano <rainearcher@g.ucla.edu>
2024-01-14 12:54:41 -0800

    move word files to dedicated folder

--- app/wordish/server.tsx
+++ app/wordish/server.tsx
@@ -1,8 +1,9 @@
 'use server'
 import { promises as fs } from "fs";
+import path from "path";
 
 export async function getRandomWord(): Promise<string> {
-    const file = await fs.readFile('/answers.txt', 'utf8');
+    const file = await fs.readFile(path.join(process.cwd(), 'wordish-files/answers.txt'), 'utf8');
     const answers = file.split('\n');
     const answer = answers[Math.floor(Math.random() * answers.length)].trim();
     console.log(answer);
@@ -10,7 +11,7 @@ export async function getRandomWord(): Promise<string> {
 }
 
 export async function isValidWord(word: string): Promise<boolean> {
-    const file = await fs.readFile('/words.txt', 'utf8');
+    const file = await fs.readFile(path.join(process.cwd(), 'wordish-files/answers.txt'), 'utf8');
     const words = file.split(/\r*\n/);
     return words.includes(word.toLowerCase());
 }
