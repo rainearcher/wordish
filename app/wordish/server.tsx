efb3182830f31826fc2fee21b5a2a9794845de01
Raine Soriano <rainearcher@g.ucla.edu>
2024-01-14 12:43:15 -0800

    Move word files to public folder to remove db dependency

--- app/wordish/server.tsx
+++ app/wordish/server.tsx
@@ -1,17 +1,16 @@
 'use server'
-import { sql } from "@vercel/postgres";
-import { unstable_noStore as noStore } from "next/cache";
+import { promises as fs } from "fs";
 
 export async function getRandomWord(): Promise<string> {
-    noStore();
-    const { rows } = await sql`SELECT word FROM answers order by random() limit 1`;
-    const answer = rows[0].word;
+    const file = await fs.readFile(process.cwd() + '/public/answers.txt', 'utf8');
+    const answers = file.split('\n');
+    const answer = answers[Math.floor(Math.random() * answers.length)].trim();
     console.log(answer);
     return answer;
 }
 
 export async function isValidWord(word: string): Promise<boolean> {
-    noStore();
-    const { rows } = await sql`SELECT word FROM words WHERE word=${word.toLowerCase()}`;
-    return rows.length != 0;
+    const file = await fs.readFile(process.cwd() + '/public/words.txt', 'utf8');
+    const words = file.split(/\r*\n/);
+    return words.includes(word.toLowerCase());
 }
