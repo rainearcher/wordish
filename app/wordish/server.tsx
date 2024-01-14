2988e9cecdfa64dd5ac4577d228673edb2e77799
Raine Soriano <rainearcher@g.ucla.edu>
2024-01-14 14:00:21 -0800

    Hard code word and answer arrays

--- app/wordish/server.tsx
+++ app/wordish/server.tsx
@@ -1,17 +1,13 @@
 'use server'
-import { promises as fs } from "fs";
-import path from "path";
+import { words } from "./words";
+import { answers } from "./answers";
 
 export async function getRandomWord(): Promise<string> {
-    const file = await fs.readFile(path.join(process.cwd(), 'wordish-files/answers.txt'), 'utf8');
-    const answers = file.split('\n');
     const answer = answers[Math.floor(Math.random() * answers.length)].trim();
     console.log(answer);
     return answer;
 }
 
 export async function isValidWord(word: string): Promise<boolean> {
-    const file = await fs.readFile(path.join(process.cwd(), 'wordish-files/answers.txt'), 'utf8');
-    const words = file.split(/\r*\n/);
     return words.includes(word.toLowerCase());
 }
